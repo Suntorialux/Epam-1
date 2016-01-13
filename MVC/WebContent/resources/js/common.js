fa346d323264287ba9a2b129e8deac3c2bf8d25a
praize <prz2000bb>
2016-01-13 23:46:56 +0300

    MVC1

--- MVC/WebContent/resources/js/common.js
+++
@@ -1,7 +0,0 @@
-function sendForm(sRef,nameForm) {
-	document.testForm.select.value=sRef;
-	document.testForm.submit();
-	for(i=0;i<nameForm.val.length;i++){
-		PtestForm.flag[i].value=nameForm.val[i].value;
-	}
-}
